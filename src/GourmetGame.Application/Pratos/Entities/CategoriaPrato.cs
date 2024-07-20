df0685f50ea7c574f9491ca766e30cfa3fb2988b
Adrian Procopiou <adrianprocopiou@gmail.com>
2024-07-20 13:54:12 -0400

    Adiciona as Entidades de CategoriaPrato e Prato
    - Adiciona a Entidade CategoriaPrato que irá descrever a categoria do Prato quando é feita a pergunta: "{Prato x} é ___{CategoriaPrato}___ mas {Prato y} não"
    - Adiciona a Entidade Prato que irá descrever o Prato informado pelo usuário quando é feita a pergunta: "Qual prato você pensou?"

---
+++ src/GourmetGame.Application/Pratos/Entities/CategoriaPrato.cs
@@ -0,0 +1,17 @@
+﻿namespace GourmetGame.Application.Pratos.Entities
+{
+    public class CategoriaPrato
+    {
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+
+        // Categoria a qual esta categoria esta associada (Categoria pai)
+        public int? CategoriaAssociadaId { get; set; }
+        public CategoriaPrato CategoriaAssociada { get; set; }
+
+        // Categorias associadas a esta categoria (Categorias filhas)
+        public ICollection<CategoriaPrato> SubCategorias { get; set; }
+
+        public ICollection<Prato> Pratos { get; set; }
+    }
+}
---
+++ src/GourmetGame.Application/Pratos/Entities/Prato.cs
@@ -0,0 +1,11 @@
+﻿namespace GourmetGame.Application.Pratos.Entities
+{
+    public class Prato
+    {
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+
+        public int CategoriaPratoId { get; set; }
+        public CategoriaPrato CategoriaPrato { get; set; }
+    }
+}
