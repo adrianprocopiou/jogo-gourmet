8121615a7107dcd88cff5f404be08ea56913c963
Adrian Procopiou <adrianprocopiou@gmail.com>
2024-07-20 14:17:00 -0400

    Adiciona o comando AdicionarCategoriaPratoCommand
    - Adiciona o comando AdicionarCategoriaPratoCommand para implementação dos testes unitários

---
+++ src/GourmetGame.Application/Pratos/Commands/AdicionarCategoriaPratoCommand/AdicionarCategoriaCommand.cs
@@ -0,0 +1,29 @@
+﻿using GourmetGame.Application.Core.Commands;
+using GourmetGame.Application.Core.Results;
+using GourmetGame.Application.Pratos.Repositories;
+using MediatR;
+
+namespace GourmetGame.Application.Pratos.Commands.AdicionarCategoriaPratoCommand
+{
+    public class AdicionarCategoriaPratoCommand : ICommand<CommandResult<Unit>>
+    {
+        public int? CategoriaAssociadaId { get; set; }
+        public required string NomeCategoria { get; set; }
+        public required string NomePrato { get; set; }
+    }
+
+    public class AdicionarCategoriaPratoCommandHandler : ICommandHandler<AdicionarCategoriaPratoCommand, CommandResult<Unit>>
+    {
+        private readonly ICategoriaPratoRepository _repository;
+
+        public AdicionarCategoriaPratoCommandHandler(ICategoriaPratoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<CommandResult<Unit>> Handle(AdicionarCategoriaPratoCommand command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
