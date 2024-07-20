8de8f33447342755f6eb6d9d71e26682515286c7
Adrian Procopiou <adrianprocopiou@gmail.com>
2024-07-20 13:49:28 -0400

    Adiciona as Classes de Erro e Result dos Comandos
    - Adiciona a classe BusinessError que irá conter os erros de regra de negócio e validações em geral
    - Adiciona a classe CommandResult que irá conter os resultados de cada commando executado, retornando os erros, ou o valor retornado pelo comando.

---
+++ src/GourmetGame.Application/Core/Errors/BusinessError.cs
@@ -0,0 +1,19 @@
+﻿namespace GourmetGame.Application.Core.Errors
+{
+    public class BusinessError
+    {
+        public string? Property { get; set; }
+        public string Message { get; }
+
+        public BusinessError(string message)
+        {
+            Message = message;
+        }
+
+        public BusinessError(string message, string property)
+        {
+            Property = property;
+            Message = message;
+        }
+    }
+}
---
+++ src/GourmetGame.Application/Core/Results/CommandResult.cs
@@ -0,0 +1,44 @@
+﻿using GourmetGame.Application.Core.Errors;
+
+namespace GourmetGame.Application.Core.Results
+{
+    public class CommandResult<TResult>
+    {
+        private CommandResult() { }
+
+        public List<BusinessError> Errors { get; private set; } = new List<BusinessError>();
+
+        public bool HasSuccess => !Errors.Any();
+
+        public TResult? Value { get; private set; }
+
+        public static CommandResult<TResult> Fail(IEnumerable<BusinessError> errors)
+        {
+            var result = new CommandResult<TResult>();
+            result.Errors.AddRange(errors);
+            return result;
+        }
+
+        public static CommandResult<TResult> Fail(BusinessError error)
+        {
+            var result = new CommandResult<TResult>();
+            result.Errors.Add(error);
+            return result;
+        }
+
+        public static CommandResult<TResult> Fail(string message)
+        {
+            return Fail(new BusinessError(message));
+        }
+
+        public static CommandResult<TResult> Fail(string message, string property)
+        {
+            return Fail(new BusinessError(property, message));
+        }
+
+        public static CommandResult<TResult> Success(TResult result)
+        {
+            return new CommandResult<TResult> { Value = result };
+        }
+    }
+}
