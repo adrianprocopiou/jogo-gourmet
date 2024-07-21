010ce0cd992097befce0273abe805eecbef5b8bb
Adrian Procopiou <adrianprocopiou@gmail.com>
2024-07-21 16:05:21 -0400

    Adiciona a Query de Busca das Categorias no BD
    - Adiciona a Query de Busca das Categorias no BD, essa query será utilizada no motor do jogo, para obter as categorias cadastradas pelo usuário quando nenhuma das categorias existentes satisfazem a necessidade do usuário.

---
+++ src/GourmetGame.Application/Pratos/Queries/ObterCategoriasPrato/ObterCategoriasPratoQuery.cs
@@ -0,0 +1,30 @@
+﻿using GourmetGame.Application.Core.Queries;
+using GourmetGame.Application.Pratos.Entities;
+using GourmetGame.Application.Pratos.Repositories;
+
+namespace GourmetGame.Application.Pratos.Queries.ObterCategoriaPrato
+{
+    public class ObterCategoriasPratoQuery : IQuery<List<CategoriaPrato>>
+    {
+        /// <summary>
+        /// Caso seja informado true, retorna apenas as categorias que não são associadas a outras categorias. Ou seja, que não são Subcategorias.
+        /// </summary>
+        public bool ApenasCategoriasPrincipais { get; set; }
+    }
+
+    public class ObterCategoriasPratoQueryHandler : IQueryHandler<ObterCategoriasPratoQuery, List<CategoriaPrato>>
+    {
+        private readonly ICategoriaPratoRepository _repository;
+
+        public ObterCategoriasPratoQueryHandler(ICategoriaPratoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<CategoriaPrato>> Handle(ObterCategoriasPratoQuery query, CancellationToken cancellationToken)
+        {
+            return await _repository
+                .GetCategoriasPratoAsync(query.ApenasCategoriasPrincipais, cancellationToken);
+        }
+    }
+}
