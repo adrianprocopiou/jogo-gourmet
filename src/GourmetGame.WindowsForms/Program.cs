861b7e5dbc9536567e10a24da1ec80ce4fc59dd9
Adrian Procopiou <adrianprocopiou@gmail.com>
2024-07-21 16:23:11 -0400

    Adiciona o Container de Injeção de Dependência
    - Adiciona o Container de Injeção de Dependência, e registra as depencias do projeto

--- src/GourmetGame.WindowsForms/Program.cs
+++ src/GourmetGame.WindowsForms/Program.cs
@@ -1,3 +1,13 @@
+using GourmetGame.Application.Core.Dispatchers;
+using GourmetGame.Application.Core.Senders;
+using GourmetGame.Application.Data.Contexts;
+using GourmetGame.Application.Pratos.Repositories;
+using GourmetGame.WindowsForms.Core.DisplayMessage;
+using GourmetGame.WindowsForms.Core.InputDialogBox;
+using GourmetGame.WindowsForms.Services;
+using Microsoft.Extensions.DependencyInjection;
+using ApplicationContext = GourmetGame.Application.Data.Contexts.ApplicationContext;
+
 namespace GourmetGame.WindowsForms
 {
     internal static class Program
@@ -11,7 +21,29 @@ namespace GourmetGame.WindowsForms
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            System.Windows.Forms.Application.Run(new FrmPrincipal());
+
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            using (ServiceProvider serviceProvider = services.BuildServiceProvider())
+            {
+                System.Windows.Forms.Application.Run(serviceProvider.GetRequiredService<FrmPrincipal>());
+            }
+        }
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services
+                .AddScoped<IGourmetGameService, GourmetGameService>()
+                .AddScoped<FrmPrincipal>()
+                .AddScoped<IDisplayMessageService, DisplayMessageService>()
+                .AddScoped<IDispatcher, Dispatcher>()
+                .AddScoped<IInputDialogBox, InputDialogBox>()
+                .AddScoped<ICategoriaPratoRepository, CategoriaPratoRepository>()
+                .AddSingleton<IApplicationContext, ApplicationContext>();
+
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(IDispatcher).Assembly);
+            });
         }
     }
 }
