886917f26d4ea2ed2c53089fb4380370b217aa13
Adrian Procopiou <adrianprocopiou@gmail.com>
2024-07-21 15:45:25 -0400

    Adiciona Dispatcher
    - Adiciona Dispatcher que será responsável por enviar os commandos e queries para os seus respectivos handlers

---
+++ src/GourmetGame.Application/Core/Dispatchers/Dispatcher.cs
@@ -0,0 +1,27 @@
+﻿using GourmetGame.Application.Core.Commands;
+using GourmetGame.Application.Core.Queries;
+using GourmetGame.Application.Core.Senders;
+using MediatR;
+
+namespace GourmetGame.Application.Core.Dispatchers
+{
+    public class Dispatcher : IDispatcher
+    {
+        private readonly IMediator mediator;
+
+        public Dispatcher(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public Task<TResponse> SendCommand<TResponse>(ICommand<TResponse> command, CancellationToken cancellationToken = default)
+        {
+            return mediator.Send(command, cancellationToken);
+        }
+
+        public Task<TResponse> SendQuery<TResponse>(IQuery<TResponse> query, CancellationToken cancellationToken = default)
+        {
+            return mediator.Send(query, cancellationToken);
+        }
+    }
+}
